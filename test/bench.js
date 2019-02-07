0ec86f48c34486830a5ca0de37e0ff23f9827dd9
Dominic Tarr <dominic.tarr@gmail.com>
2019-02-07 23:57:07 +1300

    more data from benchmark

--- test/bench.js
+++ test/bench.js
@@ -4,10 +4,32 @@ var Log = require('flumelog-offset')
 var Index = require('../')
 var codec = require('flumecodec')
 
-require('test-flumeview-index/bench')(function (file, seed) {
-  return Flume(Log(file+'log.offset', 1024, codec.json))
-    .use('index', Index(1, function (e) { return [e.key] }))
+var decodes = 0, time = 0, start = Date.now()
+var codec = {
+  encode: function (o) {
+    var s = JSON.stringify(o)
+    return s
+  },
+  decode: function (s) {
+    decodes ++
+//    var start = Date.now()
+    var start = process.hrtime()
+    var v = JSON.parse(s.toString())
+    time += process.hrtime(start)[1]
+  //  time += Date.now()-start
+    return v
+  },
+  buffer: false,
+}
+
+process.on('exit', function () {
+  console.error('memory', process.memoryUsage())
 })
 
+
+require('test-flumeview-index/bench')(function (file, seed) {
+  return Flume(Log(file+'log.offset', 1024, codec))
+    .use('index', Index(1, function (e) { return [e.key] }))
+}, 5e4)
 
 
