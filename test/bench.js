3e34324dcfdaa833351e6e6e15fd65034aed4b6e
Christian Bundy <christianbundy@fraction.io>
2020-06-04 13:41:49 -0700

    Add callback to finish `test/bench`
    Problem: The benchmark isn't a test, so running it with `tape` doesn't
    work.
    
    Solution: Turn it into a test and use a callback to trigger that the
    test eventually ends.

--- test/bench.js
+++ test/bench.js
@@ -1,6 +1,7 @@
 var Flume = require('flumedb')
 var Log = require('flumelog-offset')
 var Index = require('../')
+var tape = require('tape')
 
 var codec = {
   encode: function (o) {
@@ -16,11 +17,13 @@ process.on('exit', function () {
   console.error('memory', process.memoryUsage())
 })
 
-require('test-flumeview-index/bench')(function (file, seed) {
-  return Flume(Log(file + 'log.offset', 1024, codec)).use(
-    'index',
-    Index(1, function (e) {
-      return [e.key]
-    })
-  )
-}, 5e4)
+tape('benchmark', (t) => {
+  require('test-flumeview-index/bench')(function (file, seed) {
+    return Flume(Log(file + 'log.offset', 1024, codec)).use(
+      'index',
+      Index(1, function (e) {
+        return [e.key]
+      })
+    )
+  }, 5e4, t.end)
+})
