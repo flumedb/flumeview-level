400992679f6e57dc1ffde0ee548a9851a5fd8deb
Christian Bundy <christianbundy@fraction.io>
2020-06-04 16:16:45 -0700

    Fix test script

--- test/bench.js
+++ test/bench.js
@@ -1,29 +1,13 @@
+
 var Flume = require('flumedb')
 var Log = require('flumelog-offset')
 var Index = require('../')
-var tape = require('tape')
+var codec = require('flumecodec')
 
-var codec = {
-  encode: function (o) {
-    return JSON.stringify(o)
-  },
-  decode: function (s) {
-    return JSON.parse(s.toString())
-  },
-  buffer: false
-}
-
-process.on('exit', function () {
-  console.error('memory', process.memoryUsage())
+require('test-flumeview-index/bench')(function (file, seed) {
+  return Flume(Log(file+'log.offset', 1024, codec.json))
+    .use('index', Index(1, function (e) { return [e.key] }))
 })
 
-tape('benchmark', (t) => {
-  require('test-flumeview-index/bench')(function (file, seed) {
-    return Flume(Log(file + 'log.offset', 1024, codec)).use(
-      'index',
-      Index(1, function (e) {
-        return [e.key]
-      })
-    )
-  }, 5e4, t.end)
-})
+
+
