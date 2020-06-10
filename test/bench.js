c6fa19c6742e3a511c122357344debcc4a72bf3d
Christian Bundy <christianbundy@fraction.io>
2020-06-10 12:02:41 -0700

    Fix file path problem
    Problem: We forgot a `/` in one of our paths, which was breaking the
    benchmark test.
    
    Solution: Use the correct path with a `/`.

--- test/bench.js
+++ test/bench.js
@@ -5,7 +5,7 @@ var Index = require('../')
 var codec = require('flumecodec')
 
 require('test-flumeview-index/bench')(function (file, seed) {
-  return Flume(Log(file+'log.offset', 1024, codec.json))
+  return Flume(Log(file+'/log.offset', 1024, codec.json))
     .use('index', Index(1, function (e) { return [e.key] }))
 })
 
